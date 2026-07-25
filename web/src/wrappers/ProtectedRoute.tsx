6e92fe346c2ca5b9aa505cb2f47a47d3d48eef04
Kai Ward <wardkai558@gmail.com>
2026-07-25 18:11:18 +1000

    Built protected route

---
+++ web/src/wrappers/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+type ProtectedRouteProps = {
+    children: React.ReactNode;
+}
+
+export const ProtectedRoute = ({children}: ProtectedRouteProps) => {
+    const { loggedIn } = useAuth();
+    if (!loggedIn){
+        return (
+            <Navigate to = "/login"/>
+        )
+    }
+    return (
+        <>{children}</>
+    )
+}
