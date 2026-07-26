3b1702b0ce6c862cb69c585b4106f1d90060810d
Kai Ward <wardkai558@gmail.com>
2026-07-26 16:52:59 +1000

    Updated registerpage with real register and login logic.

--- web/src/pages/loginPage.tsx
+++ web/src/pages/loginPage.tsx
@@ -1,53 +1,48 @@
-import { useState } from 'react';
-import { useAuth } from '../wrappers/AuthContext';
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { useAuth } from "../wrappers/AuthContext";
+import { useNavigate } from "react-router-dom";
 
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-function LoginPage(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { login } = useAuth();
-    const navigate = useNavigate();
-
-    const handleSubmit = async(e: any)=>{
-        e.preventDefault();
-        try {
-            await login(email, password);
-            navigate('/dashboard')
-        } catch (error) {
-            console.error('login failed')
-            throw error
-        }
-       
-
+  const handleSubmit = async () => {
+    try {
+      await login(email, password);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("login failed", error);
     }
+  };
 
+  return (
+    <div>
+      <h1>Login</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={(e) => {handleSubmit(e)}}>
-
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password" 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                />
-
-                <button type="submit">Login</button>
-                
-            </form>
-
-        </div>
-    )
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
-
 
 export default LoginPage;
--- web/src/pages/registerPage.tsx
+++ web/src/pages/registerPage.tsx
@@ -2,54 +2,64 @@ import { useState } from "react";
 import { useAuth } from "../wrappers/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-function RegisterPage(){
-    const [email, setEmail ] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
-    const { login } = useAuth();
-    const navigate = useNavigate();
+function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const { login, register } = useAuth();
 
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={(e)=> {
-                e.preventDefault()
-                if(confirmPassword !== password){
-                    console.log(`Placeholder error passwords don't match`)
-                } else {
-                    console.log(`Placeholder success passwords match`)
-                    login();
-                    navigate('/dashboard');
-                }
-            }}> 
+  const handleSubmit = async () => {
+      try {
+        await register(email, password)
+        await login(email, password);
+        navigate('/dashboard')
+      } catch (error) {
+        console.error("Registration failed", error);
+      }
+    
+  };
 
-            <input
-            type = "email"
-            placeholder="Email"
-            value = {email}
-            onChange={(e)=> setEmail(e.target.value)}
-            />
+  return (
+    <div>
+      <h1>Register</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+         if (confirmPassword !== password) {
+            console.log(`Placeholder error passwords don't match`);
+            return;
+          } else {
+            handleSubmit();
+          }
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-            <input
-            type = "password"
-            placeholder="Password"
-            value = {password}
-            onChange={(e)=> setPassword(e.target.value)}
-            />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-            <input
-            type = "password"
-            placeholder="Confirm Password"
-            value = {confirmPassword}
-            onChange={(e)=> setConfirmPassword(e.target.value)}
-            />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
 
-            <button type = "submit">Register</button>
-
-            </form>
-        </div>
-    )
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
